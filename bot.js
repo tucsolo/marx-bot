334dcfc3240c249666f2cb67d748a8a3cf772865
Alberto Xamin <alberto@xamin.it>
2020-11-26 14:03:28 +0100

    more weird phrases

--- bot.js
+++ bot.js
@@ -13,20 +13,22 @@ bot.use(rateLimit({
 	window: 60 * 60 * 1000,
 	limit: 10,
 	onLimitExceeded: (ctx, next) => {
-		let timeNow = Date.now();
-		let sinceLast = timeNow - lastTime;
-		let probOfThisMessage = 1 - Math.exp(-messageRate * sinceLast);
-		if (Math.random() <= probOfThisMessage) {
-			lastTime = timeNow;
-			next();
-		} else if (sinceLast < 10000) {
-			let msg = ctx.message.text || ctx.message.caption;
-			if (msg) {
-				let nms = marxify(msg);
-				if (nms.localeCompare(msg) != 0)
-					ctx.reply('State sprecando il NOSTRO tempo, abbiamo anche altro da fare oltre che correggere messaggi.');
+		try {
+			let timeNow = Date.now();
+			let sinceLast = timeNow - lastTime;
+			let probOfThisMessage = 1 - Math.exp(-messageRate * sinceLast);
+			if (Math.random() <= probOfThisMessage) {
+				lastTime = timeNow;
+				next();
+			} else if (sinceLast < 10000) {
+				let msg = ctx.message.text || ctx.message.caption;
+				if (msg) {
+					let nms = marxify(msg);
+					if (nms.localeCompare(msg) != 0)
+						ctx.reply('State sprecando il NOSTRO tempo, abbiamo anche altro da fare oltre che correggere messaggi.');
+				}
 			}
-		}
+		}catch(e){ console.log(e)}
 	}
 }));
 
@@ -39,8 +41,10 @@ bot.on(['message', 'video', 'photo'], (ctx) => {
 			let simpsonref = msg.replace(/ /gi, '');
 			if (simpsonref.toLowerCase().includes('unionesovietica?manonsieradisciolta?')) ctx.reply('Si, è questo che volevamo farvi credere *preme bottone*');
 			if (simpsonref.toLowerCase().includes('femmin')) ctx.replyWithMarkdown('Sembra che tu abbia utilizzato la parola con la F, non farlo in quanto può essere sessista.', Extra.inReplyTo(ctx.message.message_id));
-			if (simpsonref.toLowerCase().includes('donna')) ctx.reply('Sembra che tu abbia utilizzato la parola con la D, non farlo in quanto può essere sessista.', Extra.inReplyTo(ctx.message.message_id));
-			if (simpsonref.toLowerCase().includes('donne')) ctx.reply('Sembra che tu abbia utilizzato la parola con la D, non farlo in quanto può essere sessista.', Extra.inReplyTo(ctx.message.message_id));
+			if (simpsonref.toLowerCase().includes('donna') || simpsonref.toLowerCase().includes('donne')) ctx.reply('Sembra che tu abbia utilizzato la parola con la D, non farlo in quanto può essere sessista.', Extra.inReplyTo(ctx.message.message_id));
+			if (simpsonref.toLowerCase().includes('ragazza') || simpsonref.toLowerCase().includes('ragazze')) ctx.reply('Sembra che tu abbia utilizzato la parola con la R, non farlo in quanto può essere sessista.', Extra.inReplyTo(ctx.message.message_id));
+			if (simpsonref.toLowerCase().includes('negro') || simpsonref.toLowerCase().includes('nigg')) ctx.reply('Sembra che tu abbia utilizzato la parola con la N, non farlo in quanto può essere razzista.', Extra.inReplyTo(ctx.message.message_id));
+			if (simpsonref.toLowerCase().includes('@matteounitn')) ctx.reply('Sembra che tu abbia menzionato @matteounitn, bravo, fallo perchè si vede che qualcosa non va.', Extra.inReplyTo(ctx.message.message_id));
 			let nms = marxify(msg);
 			if (nms.localeCompare(msg) != 0)
 				ctx.reply(nms + '*', Extra.inReplyTo(ctx.message.message_id)); // Inviamo la NOSTRA correzzione
